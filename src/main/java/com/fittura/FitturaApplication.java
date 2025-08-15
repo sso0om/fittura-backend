660ac8fa6e1ba659bd4700360d577e108cc43c82
sso0om <ssooom142@gmail.com>
2025-08-15 11:14:24 +0900

    chore: 프로젝트 생성 및 필수 디펜던시 추가

---
+++ src/main/java/com/fittura/FitturaApplication.java
@@ -0,0 +1,11 @@
+package com.fittura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitturaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitturaApplication.class, args);
+    }
+}
