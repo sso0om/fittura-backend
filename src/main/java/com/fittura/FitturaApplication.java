86aecde00dbb6e2bf108c2799a64e5aac9176186
sso0om <ssooom142@gmail.com>
2025-08-22 00:31:50 +0900

    [Feat] 멤버 엔티티, 레파지토리
    * feat: BaseEntity
    * feat: Member, MemberRepository, Role
    * refactor: 코드래빗 반영
    * refactor: 코드래빗 반영2

--- src/main/java/com/fittura/FitturaApplication.java
+++ src/main/java/com/fittura/FitturaApplication.java
@@ -2,8 +2,10 @@ package com.fittura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FitturaApplication {
     public static void main(String[] args) {
         SpringApplication.run(FitturaApplication.class, args);
---
+++ src/main/java/com/fittura/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.fittura.domain.member.repository;
+
+import com.fittura.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
