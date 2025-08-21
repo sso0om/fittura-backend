45ab2d01e7cfdba2ee8e8c4cc15b05954677bdec
sso0om <ssooom142@gmail.com>
2025-08-22 02:01:17 +0900

    [Feat] JWT 토큰 발급/검증 구현 (#26)
    * chore: jwt 의존성 추가
    
    * chore: jwt 설정 추가
    
    * feat: JwtTokenProvider
    
    * refactor: 코드래빗 반영
    
    * refactor: 코드래빗 반영2

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,21 +25,36 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // OpenAPI/Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+    // Common utils
     implementation("org.apache.commons:commons-lang3:3.18.0")
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
+    // Dev tools
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+    // DB Drivers
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
 
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
