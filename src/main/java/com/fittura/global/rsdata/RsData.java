c3cb724be49b2aca4cf14fc6dddd118cdafae1a8
sso0om <ssooom142@gmail.com>
2025-08-23 23:05:34 +0900

    feat: 공통 응답 처리

---
+++ src/main/java/com/fittura/global/rsdata/RsData.java
@@ -0,0 +1,38 @@
+package com.fittura.global.rsdata;
+
+import com.fittura.global.error.ErrorCode;
+import com.fittura.global.error.ValidationError;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record RsData<T>(
+        int status,
+        String code,
+        String msg,
+        T data
+) {
+    public static RsData<Void> success() {
+        return new RsData<>(HttpStatus.OK.value(), "S-01", "요청이 성공적으로 처리되었습니다.", null);
+    }
+
+    public static <T> RsData<T> success(T data) {
+        return new RsData<>(HttpStatus.OK.value(), "S-01", "요청이 성공적으로 처리되었습니다.", data);
+    }
+
+    public static <T> RsData<T> success(String msg, T data) {
+        return new RsData<>(HttpStatus.OK.value(), "S-01", msg, data);
+    }
+
+    public static RsData<Void> error(ErrorCode errorCode) {
+        return new RsData<>(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    public static <T> RsData<T> error(ErrorCode errorCode, T data) {
+        return new RsData<>(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage(), data);
+    }
+
+    public static RsData<List<ValidationError>> validationError(ErrorCode errorCode, List<ValidationError> errors) {
+        return new RsData<>(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage(), errors);
+    }
+}
