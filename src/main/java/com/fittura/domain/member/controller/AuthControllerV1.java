a836730078cd1e09cab3225c6617b4fc59b1dbac
sso0om <ssooom142@gmail.com>
2026-04-12 11:03:12 +0900

    refactor: 컨트롤러 쿠키 설정 reponseEntity로 바로 반환

--- src/main/java/com/fittura/domain/member/controller/AuthControllerV1.java
+++ src/main/java/com/fittura/domain/member/controller/AuthControllerV1.java
@@ -1,7 +1,6 @@
 package com.fittura.domain.member.controller;
 
 import com.fittura.domain.member.dto.AuthResultDto;
-import com.fittura.domain.member.dto.TokenDto;
 import com.fittura.domain.member.dto.request.SignInReqDto;
 import com.fittura.domain.member.dto.request.SignUpReqDto;
 import com.fittura.domain.member.dto.response.AuthResDto;
@@ -9,7 +8,6 @@ import com.fittura.domain.member.service.AuthService;
 import com.fittura.global.rsdata.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -29,76 +27,56 @@ public class AuthControllerV1 {
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "회원가입 API")
     public ResponseEntity<RsData<AuthResDto>> signUp(
-        @RequestBody @Valid SignUpReqDto signUpReqDto,
-        HttpServletResponse httpServletResponse
+        @RequestBody @Valid SignUpReqDto signUpReqDto
     ) {
         AuthResultDto resultDto = authService.signUp(signUpReqDto);
-        AuthResDto resDto = processAuthResult(resultDto, httpServletResponse);
+        ResponseCookie cookie = authService.generateRefreshTokenCookie(resultDto.tokenDto());
+        AuthResDto resDto = AuthResDto.of(resultDto);
 
         return ResponseEntity
             .status(HttpStatus.CREATED)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
             .body(RsData.createSuccess("회원가입이 완료되었습니다.", resDto));
     }
 
     @PostMapping("/signin")
     @Operation(summary = "로그인", description = "로그인 API")
     public ResponseEntity<RsData<AuthResDto>> signIn(
-        @RequestBody @Valid SignInReqDto signInReqDto,
-        HttpServletResponse httpServletResponse
+        @RequestBody @Valid SignInReqDto signInReqDto
     ) {
         AuthResultDto resultDto = authService.signIn(signInReqDto);
-        AuthResDto resDto = processAuthResult(resultDto, httpServletResponse);
+        ResponseCookie cookie = authService.generateRefreshTokenCookie(resultDto.tokenDto());
+        AuthResDto resDto = AuthResDto.of(resultDto);
 
         return ResponseEntity
             .status(HttpStatus.OK)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
             .body(RsData.success("로그인되었습니다.", resDto));
     }
 
     @PostMapping("/reissue")
     @Operation(summary = "토큰 재발급", description = "토큰 재발급 API")
     public ResponseEntity<RsData<AuthResDto>> reissueTokens(
-        @CookieValue(name = "${app.cookie.refresh-token-name}") String refreshToken,
-        HttpServletResponse httpServletResponse
+        @CookieValue(name = "${app.cookie.refresh-token-name}") String refreshToken
     ) {
         AuthResultDto resultDto = authService.reissueTokens(refreshToken);
-        AuthResDto resDto = processAuthResult(resultDto, httpServletResponse);
+        ResponseCookie cookie = authService.generateRefreshTokenCookie(resultDto.tokenDto());
+        AuthResDto resDto = AuthResDto.of(resultDto);
 
         return ResponseEntity
             .status(HttpStatus.OK)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
             .body(RsData.success("토큰이 재발급되었습니다.", resDto));
     }
 
     @PostMapping("/logout")
     @Operation(summary = "로그아웃", description = "로그아웃 API")
-    public ResponseEntity<RsData<Void>> logout(
-        HttpServletResponse httpServletResponse
-    ) {
+    public ResponseEntity<RsData<Void>> logout() {
         ResponseCookie cookie = authService.createLogoutCookie();
-        httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
         return ResponseEntity
             .status(HttpStatus.OK)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
             .body(RsData.success("로그아웃되었습니다.", null));
-    }
-
-
-    // ========== 헬퍼 메서드 ==========
-
-    /**
-     * 회원가입 또는 로그인 성공 후 공통으로 수행되는 후처리 작업을 담당
-     * 1. Refresh Token 쿠키를 HttpServletResponse에 추가
-     * 2. Access Token을 포함한 AuthResDto를 생성하여 반환
-     *
-     * @param authResultDto       인증 서비스의 결과(사용자 정보, 토큰)를 담은 DTO
-     * @param httpServletResponse 쿠키를 헤더에 추가하기 위한 서블릿 응답 객체
-     * @return                    API 응답 본문에 포함될 DTO
-     */
-    private AuthResDto processAuthResult(AuthResultDto authResultDto, HttpServletResponse httpServletResponse) {
-        TokenDto tokenDto = authResultDto.tokenDto();
-
-        ResponseCookie cookie = authService.generateRefreshTokenCookie(tokenDto);
-        httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-
-        return AuthResDto.of(authResultDto);
     }
 }
