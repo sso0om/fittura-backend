d6c265abaad250b99162f21e2d45c0c66048867e
sso0om <ssooom142@gmail.com>
2026-06-18 15:46:21 +0900

    chore: dto validation 수정

--- src/main/java/com/fittura/domain/order/order/dto/request/AddressCreateReqDto.java
+++ src/main/java/com/fittura/domain/order/order/dto/request/AddressCreateReqDto.java
@@ -1,39 +1,39 @@
 package com.fittura.domain.order.order.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record AddressCreateReqDto(
     @Schema(example = "John")
-    @NotNull @Size(min = 1, max = 100)
+    @NotBlank @Size(max = 100)
     String receiverName,
 
     @Schema(example = "021231234")
-    @NotNull @Size(min = 9, max = 20)
+    @NotBlank @Size(min = 9, max = 20)
     String phoneNumber,
 
     @Schema(example = "12345")
-    @NotNull @Pattern(regexp = "\\d{5}")
+    @NotBlank @Pattern(regexp = "\\d{5}")
     String zipCode,
 
     @Schema(example = "서울특별시 중구 서소문로 127")
-    @NotNull
+    @NotBlank
     String address,
 
     @Schema(example = "시청역")
     String addressDetail,
 
     @Schema(example = "서울특별시")
-    @NotNull
+    @NotBlank @Size(max = 20)
     String sido,
 
     @Schema(example = "중구")
-    @NotNull
+    @NotBlank @Size(max = 20)
     String sigungu,
 
-    @Schema
+    @Schema(example = "문앞에 놓아주세요.")
     String deliveryMemo
 ) {
 }
