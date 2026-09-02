2c41c59bdb9330685cdf55b2f6b10718819107d7
sso0om <ssooom142@gmail.com>
2026-09-02 18:22:11 +0900

    chore: InitData 부품 카테고리 추가

--- src/main/java/com/fittura/global/initdata/CategoryInitData.java
+++ src/main/java/com/fittura/global/initdata/CategoryInitData.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Profile("dev")
+@Order(2)
 @RequiredArgsConstructor
 public class CategoryInitData implements ApplicationRunner {
 
@@ -39,6 +41,22 @@ public class CategoryInitData implements ApplicationRunner {
 
         createChild("3단 서랍장", 1, drawer);
         createChild("5단 서랍장", 2, drawer);
+
+
+        // ===== 부품 (루트별) =====
+        Category tableParts = createChild("부품", 3, table);
+        createChild("식탁 상판", 1, tableParts);
+        createChild("식탁 다리", 2, tableParts);
+
+        Category chairParts = createChild("부품", 3, chair);
+        createChild("의자 좌판", 1, chairParts);
+        createChild("의자 등받이", 2, chairParts);
+        createChild("의자 다리", 3, chairParts);
+
+        Category drawerParts = createChild("부품", 3, drawer);
+        createChild("서랍장 상판", 1, drawerParts);
+        createChild("서랍장 서랍", 2, drawerParts);
+        createChild("서랍장 손잡이", 3, drawerParts);
     }
 
     private Category createRoot(String name, int order) {
