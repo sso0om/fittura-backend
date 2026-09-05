6601665eac280c01c8e43296f1d5b8f0d83c8850
sso0om <ssooom142@gmail.com>
2026-09-05 16:50:09 +0900

    refactor: 주문 목록 조회 시 일자 조건 LocalDateTime -> LocalDate로 변경

--- src/main/java/com/fittura/domain/order/order/dto/request/OrderSearchCondition.java
+++ src/main/java/com/fittura/domain/order/order/dto/request/OrderSearchCondition.java
@@ -3,17 +3,26 @@ package com.fittura.domain.order.order.dto.request;
 import com.fittura.domain.order.order.error.OrderErrorCode;
 import com.fittura.global.exception.ServiceException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record OrderSearchCondition(
     String orderNumber,
     String productName,
-    LocalDateTime startDate,
-    LocalDateTime endDate
+    LocalDate startDate,
+    LocalDate endDate
 ) {
     public OrderSearchCondition {
         if (startDate.isAfter(endDate)) {
             throw new ServiceException(OrderErrorCode.DATE_RANGE_INVALID);
         }
+    }
+
+    public LocalDateTime startDateTime() {
+        return startDate.atStartOfDay();
+    }
+
+    public LocalDateTime endDateTime() {
+        return endDate.plusDays(1).atStartOfDay();
     }
 }
