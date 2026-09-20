e72dc6d9cae373e5228cb1e32f10bcc490e64371
sso0om <ssooom142@gmail.com>
2026-09-20 21:15:23 +0900

    feat: DeliveryType  무료 배송 기준 추가

--- src/main/java/com/fittura/domain/product/product/constant/DeliveryType.java
+++ src/main/java/com/fittura/domain/product/product/constant/DeliveryType.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum DeliveryType {
-    PARCEL(4000L),
-    INSTALLATION(30_000L);
+    PARCEL(4_000L, 40_000L),
+    INSTALLATION(30_000L, null);
 
     private final Long baseFee;
+    private final Long freeShippingThreshold;
 }
